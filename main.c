ab45804999a511451ddbe5f215ee96bbf2fdc100
Hafiz Aqilla Subagyo <muhammad.hafiz22@ui.ac.id>
2024-05-28 15:12:05 +0700

    Menambahkan fitur fungsi menu tampilan di awal

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+#define NUM_ITEMS 10
+
+//enih bagian restocking punya hapis
+
+typedef struct {
+    char name [30];
+    int stock;
+    float price;
+}Item;
+
+void MenuTampilan(){ //Fungsi untuk menampilkan display untuk memilih kegiatan yang ingin dilakukan
+    printf("1. Tampilkan item\n");
+    printf("2. Restock barang\n");
+    printf("3. Tampilkan modal\n");
+    printf("4. Keluar\n");
+    printf("Masukkan pilihan anda: ");
+}
