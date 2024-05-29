ffab0cabb6b5ab2f07a75becbfd0d85e6950fbaa
beanjeans <josephine.sere@ui.ac.id>
2024-05-29 17:06:24 +0700

    gabungan

--- data pemasukan barang.c
+++ data pemasukan barang.c
@@ -25,14 +25,17 @@ int main(void) {
 
     DataBarang *barang = (DataBarang *)malloc(n * sizeof(DataBarang));
 
+    //Tampilan database
+    void PrintDatabase();
+
     // Input data pemasukan
-    inputData(barang, n);
+    inputData(barang, n, Database);
+    InitializeDatabase(&barangdummy);
 
     // Output data pemasukan
     printf("--DATA PEMASUKAN %s--\n", barang[0].tanggal);
     for (int i = 0; i < n; i++) {
-        printf("%d. ", i + 1);
-        printf("Nama Barang: %s\n", barang[i].nama);
+        printf("Nomor Barang: %d", barang[i].nama);
         printf("Jumlah Barang: %d\n", barang[i].jumlah);
         printf("Harga Barang: %d\n", barang[i].harga);
         printf("Total Harga Barang: %d\n", barang[i].total_harga);
@@ -44,24 +47,21 @@ int main(void) {
     return 0;
 }
 
-void inputData(DataBarang *barang, int jumlah_tipebarang) {
+void inputData(DataBarang *barang, int jumlah_tipebarang, Database) {
     printf("Masukkan tanggal pembelian: ");
     fgets(barang[0].tanggal, BUFFER_SIZE, stdin);
     barang[0].tanggal[strcspn(barang[0].tanggal, "\n")] = '\0';
 
-    for (int i = 0; i < jumlah_tipebarang; i++) {
-        printf("%d. Masukkan nama barang: ", i + 1);
-        fgets(barang[i].nama, BUFFER_SIZE, stdin);
-        barang[i].nama[strcspn(barang[i].nama, "\n")] = '\0';
+    printf("Masukkan nomor barang: ");
+    scanf("%d", &Database.items[]);
+    
+    printf("Masukkan jumlah barang: ");
+    scanf("%d", &(barang.jumlah));
 
-        printf("Masukkan jumlah barang: ");
-        scanf("%d", &(barang[i].jumlah));
+    printf("Masukkan harga barang: ");
+    scanf("%d", &(barang.harga));
 
-        printf("Masukkan harga barang: ");
-        scanf("%d", &(barang[i].harga));
-
-        barang[i].total_harga = barang[i].jumlah * barang[i].harga;
-        getchar();
-        printf("\n");
-    }
+    barang[i].total_harga = barang[i].jumlah * barang[i].harga;
+    getchar();
+    printf("\n");
 }
